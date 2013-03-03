b70f2481b9e73992eba1ba8ccdb6ec20e65148ed
ye.liy <ye.liy@qq.com>
2013-03-03 16:17:14 +0800

    aa

---
+++ src/main/java/me/liye/tekken/wiki/Config.java
@@ -0,0 +1,50 @@
+package me.liye.tekken.wiki;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/*
+ * @author <a href="mailto:ye.liy@alibaba-inc.com">ye.liy</a>
+ */
+public class Config {
+
+    private static final String DEF_DBFILE = "/Users/liye/mywork/tekken-app/src/main/resources/tekken.sqlite";
+    private static final String KEY_DBFILE = "dbFile";
+    public static String        dbFile;
+    static {
+        File cfg = new File(System.getProperty("user.home"), "tekken-app.properties");
+        Properties p = new Properties();
+        if (!cfg.exists()) {
+            dbFile = DEF_DBFILE;
+            p.setProperty(KEY_DBFILE, dbFile);
+            try {
+                p.store(new FileOutputStream(cfg), "");
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                p.load(new FileInputStream(cfg));
+                dbFile = p.getProperty(KEY_DBFILE);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Config.dbFile);
+    }
+}
